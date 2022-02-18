1bc9e3ae479a4463c9d485bc8e2bb76419a87ca6
Uzair Ali <uzairali001@gmail.com>
2022-02-19 01:19:01 +0500

    Feat: Guid. DateTime converter added

---
+++ UzairAli.JsonConverters/JsonStringDateTimeConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace UzairAli.JsonConverters;
+public class JsonStringDateTimeConverter : JsonConverterFactory
+{
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeToConvert == typeof(DateTime) ||
+            typeToConvert == typeof(DateTime?);
+    }
+
+    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        return typeToConvert == typeof(DateTime)
+           ? new DateTimeConverter()
+           : new NullableDateTimeConverter();
+    }
+
+    private class DateTimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return GetValue(ref reader) ?? default;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString() ?? string.Empty);
+        }
+    }
+
+    private class NullableDateTimeConverter : JsonConverter<DateTime?>
+    {
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return GetValue(ref reader);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            if (value is null)
+            {
+                writer.WriteNullValue();
+                return;
+            }
+            writer.WriteStringValue(value.Value.ToString("O"));
+        }
+    }
+
+    private static DateTime? GetValue(ref Utf8JsonReader reader)
+    {
+        return reader.TokenType switch
+        {
+            JsonTokenType.Null => default,
+            _ => string.IsNullOrEmpty(reader.GetString()) is false ? DateTime.Parse(reader.GetString()!) : null,
+        };
+    }
+}
---
+++ UzairAli.JsonConverters/JsonStringGuidConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace UzairAli.JsonConverters;
+public class JsonStringGuidConverter : JsonConverterFactory
+{
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeToConvert == typeof(Guid) ||
+            typeToConvert == typeof(Guid?);
+    }
+
+    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        return typeToConvert == typeof(Guid)
+           ? new GuidConverter()
+           : new NullableGuidConverter();
+    }
+
+    private class GuidConverter : JsonConverter<Guid>
+    {
+        public override Guid Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return GetValue(ref reader) ?? default;
+        }
+
+        public override void Write(Utf8JsonWriter writer, Guid value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString() ?? string.Empty);
+        }
+    }
+
+    private class NullableGuidConverter : JsonConverter<Guid?>
+    {
+        public override Guid? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return GetValue(ref reader);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Guid? value, JsonSerializerOptions options)
+        {
+            if (value is null)
+            {
+                writer.WriteNullValue();
+                return;
+            }
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+
+    private static Guid? GetValue(ref Utf8JsonReader reader)
+    {
+        return reader.TokenType switch
+        {
+            JsonTokenType.Null => default,
+            _ => string.IsNullOrEmpty(reader.GetString()) is false ? new Guid(reader.GetString()!) : default,
+        };
+    }
+}

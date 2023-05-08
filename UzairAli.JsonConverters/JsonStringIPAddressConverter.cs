e38c7b35c05554e3f63f8fc8c794c6c834d3d2a8
Uzair Ali <uzairali001@gmail.com>
2023-05-09 00:05:36 +0500

    Feat: IPAddress and IPEndPoint converters added

---
+++ UzairAli.JsonConverters/JsonStringIPAddressConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace UzairAli.JsonConverters;
+
+public class JsonStringIPAddressConverter : JsonConverterFactory
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return (objectType == typeof(IPAddress));
+    }
+
+    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        return typeToConvert == typeof(IPAddress)
+            ? new IPAddressConverter()
+            : new NullableIPAddressConverter();
+    }
+
+    private class IPAddressConverter : JsonConverter<IPAddress>
+    {
+        public override IPAddress Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return IPAddress.Parse(reader.GetString());
+        }
+
+        public override void Write(Utf8JsonWriter writer, IPAddress value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+
+    private class NullableIPAddressConverter : JsonConverter<IPAddress?>
+    {
+        public override IPAddress? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string? s = reader.GetString();
+            return s is not null ? IPAddress.Parse(s) : default;
+        }
+
+        public override void Write(Utf8JsonWriter writer, IPAddress? value, JsonSerializerOptions options)
+        {
+            if (value is null)
+            {
+                writer.WriteNullValue();
+                return;
+            }
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
---
+++ UzairAli.JsonConverters/JsonStringIPEndpointConverter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+namespace UzairAli.JsonConverters;
+
+public class JsonStringIPEndPointConverter : JsonConverterFactory
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return (objectType == typeof(IPEndPoint));
+    }
+
+    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        return typeToConvert == typeof(IPEndPoint)
+            ? new IPEndPointConverter()
+            : new NullableIPEndPointConverter();
+    }
+
+    private class IPEndPointConverter : JsonConverter<IPEndPoint>
+    {
+
+        public override IPEndPoint Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            JsonObject jo = JsonNode.Parse(ref reader)!.AsObject();
+            IPAddress address = IPAddress.Parse(jo["Address"]!.GetValue<string>());
+            int port = jo["Port"]!.GetValue<int>();
+            return new IPEndPoint(address, port);
+        }
+
+        public override void Write(Utf8JsonWriter writer, IPEndPoint value, JsonSerializerOptions options)
+        {
+            JsonObject jo = new()
+            {
+                { "Address", value.Address.ToString() },
+                { "Port", value.Port }
+            };
+            jo.WriteTo(writer);
+        }
+    }
+
+    private class NullableIPEndPointConverter : JsonConverter<IPEndPoint?>
+    {
+        public override IPEndPoint? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            JsonObject? jo = JsonNode.Parse(ref reader)?.AsObject();
+            if (jo is null)
+            {
+                return default;
+            }
+            if (!jo.TryGetPropertyValue("Address", out JsonNode? jsonNodeAddress) || jsonNodeAddress is null
+                || !jo.TryGetPropertyValue("Port", out JsonNode? jsonNodePort) || jsonNodePort is null)
+            {
+                return default;
+            }
+
+
+            IPAddress address = IPAddress.Parse(jsonNodeAddress.ToString());
+            int port = jsonNodePort.GetValue<int>();
+            return new IPEndPoint(address, port);
+        }
+
+        public override void Write(Utf8JsonWriter writer, IPEndPoint? value, JsonSerializerOptions options)
+        {
+            if (value is null)
+            {
+                writer.WriteNullValue();
+                return;
+            }
+            JsonObject jo = new()
+            {
+                { "Address", value.Address.ToString() },
+                { "Port", value.Port }
+            };
+            jo.WriteTo(writer);
+        }
+    }
+}
